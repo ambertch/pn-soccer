585bb72e5253f7125d553c6d70d13a78d851f497
Ambert Ho <ambertch@gmail.com>
2014-07-02 16:49:15 -0700

    use pubnub.js for all event handling/DOM manipulation

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
-/*
-a game of single/multiplayer one dimensional soccer where the goal is to push the ball towards the opposing team's goal. 
-	- a word (generated from http://hipsum.co/) is presented for the player to enter as a command
-	- the word will change after a time interval
-	- a correctly entered command advances the ball towards the opposing team's goal
-	- an incorrectly entered command advances the ball towards your goal
-*/
-
-$(document).ready(function() {
-// (function($) {
-
+setTimeout(function() {
 	// pubnub vars
-	var gameChannel = "pubowar", pubkey = 'demo', subkey = 'demo';
+	var gameChannel = "pubnub-soccer"
+	,	pubkey = 'demo'
+	,	subkey = 'demo';
 
 	// game vars
-	var blueTeam = 1; // blue team moves left
-	var redTeam = 2; // red team moves right
-	var numBlueTeamPlayers = 0;
-	var numRedTeamPlayers = 0;
-	var $ball = $("#ball");	
-	var team = _.random(1, 2);
-	var baseMoveDistance = 75;
-	var defaultCommandInterval = 2000;	
-	var commandsPool = ["You", "probably", "haven't", "heard", "of", "them", "meggings", "Carles", "beard", "plaid", "dreamcatcher", "squid", "slow-carb", "Wes", "Anderson", "YOLO", "occupy", "single-origin", "coffee", "bespoke", "fanny", "pack", "pug", "Brooklyn", "pork", "belly", "vinyl", "banh", "mi", "+1", "fingerstache", "Tumblr", "90's", "8-bit", "cray", "keytar", "PBR&B", "banjo", "VHS", "selfies", "leggings", "deep", "v", "ugh", "mixtape", "whatever", "Selvage", "retro", "forage", "Banksy", "trust", "fund", "cardigan", "sartorial", "paleo", "semiotics", "art", "party", "tattooed", "Stumptown", "distillery", "Pinterest", "photo", "booth", "mlkshk", "four", "loko", "Bushwick", "authentic", "chia", "sriracha", "sustainable", "scenester", "lomo", "Tonx", "selvage", "Squid", "twee", "locavore", "roof", "pop-up", "Austin", "post-ironic", "actually", "kogi", "brunch", "Etsy", "Thundercats", "tofu", "Irony", "Pitchfork", "Authentic", "meh", "fap", "ennui", "fashion", "axe", "Wolf", "yr", "mustache", "swag", "letterpress", "organic", "Williamsburg", "XOXO", "Shoreditch", "Brunch", "wayfarers", "Cosby", "sweater", "gentrify", "before", "they", "sold", "out", "Seitan", "food", "truck", "kitsch", "Flexitarian", "butcher", "Roof", "typewriter", "craft", "beer", "Chia", "farm-to-table", "keffiyeh", "flexitarian", "Occupy", "Blue", "Bottle", "Truffaut", "you", "wolf", "chambray", "mumblecore", "lo-fi", "messenger", "bag", "shabby", "chic", "High", "Life", "gluten-free", "DIY", "bitters", "direct", "trade", "literally", "viral", "Portland", "Quinoa", "McSweeney's", "asymmetrical", "synth", "disrupt", "narwhal", "Pug", "put", "a", "bird", "on", "it", "American", "Apparel", "readymade", "Chillwave", "cred", "hashtag", "Bicycle", "rights", "tousled", "Schlitz", "Godard", "small", "batch", "Hashtag", "PBR", "skateboard", "raw", "denim", "iPhone", "cornhole", "pour-over", "church-key", "Put", "Echo", "Park", "drinking", "vinegar", "vegan", "Intelligentsia", "seitan", "biodiesel"]
+	var blueTeam = 1 // blue team moves left
+	,	redTeam = 2 // red team moves right
+	, 	numBlueTeamPlayers = 0
+	, 	numRedTeamPlayers = 0
+	, 	ball = PUBNUB.$("ball")
+	,   teamName = PUBNUB.$("team-name")
+	,   ballDirection = PUBNUB.$("ball-direction")
+	,	teamScore = PUBNUB.$("your-team-score")
+	,	blueTeamPlayerCount = PUBNUB.$("blue-team-player-count")
+	, 	redTeamPlayerCount = PUBNUB.$("red-team-player-count")
+	, 	playerFeedback = PUBNUB.$('player-feedback')
+	, 	commandInput = PUBNUB.$("command-input")
+	,	command = PUBNUB.$("command")
+	, 	team = _.random(1, 2)
+	, 	defaultMoveDistance = 75
+	, 	timeToGameStart = 4000	
+	,	board = {
+			width: null,
+			leftEdge: null,
+			rightEdge: null
+		}
+	, defaultCommandInterval = 2000
+	, commandsPool = ["You", "probably", "haven't", "heard", "of", "them", "meggings", "Carles", "beard", "plaid", "dreamcatcher", "squid", "slow-carb", "Wes", "Anderson", "YOLO", "occupy", "single-origin", "coffee", "bespoke", "fanny", "pack", "pug", "Brooklyn", "pork", "belly", "vinyl", "banh", "mi", "+1", "fingerstache", "Tumblr", "90's", "8-bit", "cray", "keytar", "PBR&B", "banjo", "VHS", "selfies", "leggings", "deep", "v", "ugh", "mixtape", "whatever", "Selvage", "retro", "forage", "Banksy", "trust", "fund", "cardigan", "sartorial", "paleo", "semiotics", "art", "party", "tattooed", "Stumptown", "distillery", "Pinterest", "photo", "booth", "mlkshk", "four", "loko", "Bushwick", "authentic", "chia", "sriracha", "sustainable", "scenester", "lomo", "Tonx", "selvage", "Squid", "twee", "locavore", "roof", "pop-up", "Austin", "post-ironic", "actually", "kogi", "brunch", "Etsy", "Thundercats", "tofu", "Irony", "Pitchfork", "Authentic", "meh", "fap", "ennui", "fashion", "axe", "Wolf", "yr", "mustache", "swag", "letterpress", "organic", "Williamsburg", "XOXO", "Shoreditch", "Brunch", "wayfarers", "Cosby", "sweater", "gentrify", "before", "they", "sold", "out", "Seitan", "food", "truck", "kitsch", "Flexitarian", "butcher", "Roof", "typewriter", "craft", "beer", "Chia", "farm-to-table", "keffiyeh", "flexitarian", "Occupy", "Blue", "Bottle", "Truffaut", "you", "wolf", "chambray", "mumblecore", "lo-fi", "messenger", "bag", "shabby", "chic", "High", "Life", "gluten-free", "DIY", "bitters", "direct", "trade", "literally", "viral", "Portland", "Quinoa", "McSweeney's", "asymmetrical", "synth", "disrupt", "narwhal", "Pug", "put", "a", "bird", "on", "it", "American", "Apparel", "readymade", "Chillwave", "cred", "hashtag", "Bicycle", "rights", "tousled", "Schlitz", "Godard", "small", "batch", "Hashtag", "PBR", "skateboard", "raw", "denim", "iPhone", "cornhole", "pour-over", "church-key", "Put", "Echo", "Park", "drinking", "vinegar", "vegan", "Intelligentsia", "seitan", "biodiesel"]
 
-	var board = {
-		width: $('#game-board').width(),
-		leftEdge: $('#game-board').offset().left,
-		rightEdge: $('#game-board').offset().left + $('#game-board').width()
+	function calculateBoardAttributes() {
+		var width = PUBNUB.$('game-board').offsetWidth;
+		var leftEdge = document.body.clientWidth - width;
+		var rightEdge = leftEdge + PUBNUB.$('game-board').offsetWidth;
+		board.width = width;
+		board.leftEdge = leftEdge;
+		board.rightEdge = rightEdge;
 	}
 
 	var gameOptions = {
@@ -35,40 +44,40 @@ $(document).ready(function() {
 	}	
 
 	var pubnub = PUBNUB.init({
-    publish_key: pubkey,
-    subscribe_key: subkey,
-    uuid: team + "_" + _.random(1, 9999999)
+	    publish_key: pubkey,
+	    subscribe_key: subkey,
+	    uuid: team + "_" + _.random(1, 9999999)
  	});
  
  	pubnub.subscribe({   
-    channel: gameChannel,
-   	presence : function(m){   		
-   	}, // Presence Events Emitted
-    message : function(message){
-    	console.log('received message:')
-    	console.log(message)
-    	if(message.type === "ballmove") {
-    		moveBall(message.value)
-    	} else if (message.type === "goal") {
-    		updateScore(message);
-    	}
-    },
-    connect : function(m) {
-    	// polling for presence, since a disconnect event is not fired upon exiting the game
-    	setInterval(function() {
-    		pubnub.here_now({
-    		channel : gameChannel,
-    		callback : function(presenceData) {
-    			// console.log(presenceData.uuids)
-    			numBlueTeamPlayers = 0;
-    			numRedTeamPlayers = 0;
-    			_(presenceData.uuids).each(function(uuid) {
-    				parseInt(uuid.substring(0,1)) === blueTeam ? numBlueTeamPlayers += 1 : numRedTeamPlayers += 1;
-    			});
-    			setTeamCounts()
-    		}
-    	});
-    	}, 2000);
+	    channel: gameChannel,
+	   	presence : function(m){   		
+	   	}, // Presence Events Emitted
+	    message : function(message){
+	    	console.log('received message:')
+	    	console.log(message)
+	    	if(message.type === "ballmove") {
+	    		moveBall(message.value)
+	    	} else if (message.type === "goal") {
+	    		updateScore(message);
+	    	}
+	    },
+	    connect : function(m) {
+	    	// polling for presence, since a disconnect event is not fired upon exiting the game
+	    	setInterval(function() {
+	    		pubnub.here_now({
+	    		channel : gameChannel,
+	    		callback : function(presenceData) {
+	    			// console.log(presenceData.uuids)
+	    			numBlueTeamPlayers = 0;
+	    			numRedTeamPlayers = 0;
+	    			_(presenceData.uuids).each(function(uuid) {
+	    				parseInt(uuid.substring(0,1)) === blueTeam ? numBlueTeamPlayers += 1 : numRedTeamPlayers += 1;
+	    			});
+	    			setTeamCounts()
+	    			}
+	    		});
+	    	}, 2000);
 
     }
 	});
@@ -76,48 +85,29 @@ $(document).ready(function() {
 
  	function updateScore(data) { 		
  		if (data.team == team) {
- 			var score = parseInt($("#your-team-score").html())
- 			$("#your-team-score").html(score + 1);
+ 			var score = parseInt(teamScore.innerHTML);
+ 			teamScore.innerText = score + 1;
  		}
  	}
 
 	function assignTeam() {
-		document.getElementById("team-name").innerHTML = (team === blueTeam ? "BLUE" : "RED");
-		document.getElementById("ball-direction").innerHTML = (team === blueTeam ? "left" : "right");
+		teamName.innerText = (team === blueTeam ? "BLUE" : "RED");
+		ballDirection.innerText = (team === blueTeam ? "left" : "right");
 		if (team === blueTeam) {
-			document.getElementById("team-name").className = "blue";
+			teamName.className = "blue";
 		} else {
-			document.getElementById("team-name").className = "red";
+			teamName.className = "red";
 		}
 	}	
 
 	function setTeamCounts() {
-		$("#blue-team-player-count").html(numBlueTeamPlayers);
-		$("#red-team-player-count").html(numRedTeamPlayers);
-	}
-
-	function sendMoveBallMessage(success) {
-	 	var moveDistance;
-		if (team === blueTeam) {
-			if (success) moveDistance = 0 - calculateMoveDistance();
-			else moveDistance = 0 + calculateMoveDistance();
-		} else {
-			if (success) moveDistance = 0 + calculateMoveDistance();
-			else moveDistance = 0 - calculateMoveDistance();
-		}
-		
-		pubnub.publish({
-			channel: gameChannel,
-			message: {
-				type: "ballmove",
-				value: moveDistance
-			}
-		})
+		blueTeamPlayerCount.innerText = numBlueTeamPlayers;
+		redTeamPlayerCount.innerText = numRedTeamPlayers;		
 	}
 
 	function moveBall(value) {
-		var newPos = parseInt($ball.css("left")) + value
-		$ball.css("left", newPos);
+		var newPos = parseInt(ball.offsetWidth) + value
+		PUBNUB.css(ball, { left: newPos })		
 		detectGoal(newPos);
 	}
 
@@ -140,8 +130,9 @@ $(document).ready(function() {
 					team: team				
 				}
 			})
-			$('#player-feedback').html("SCORE!!!");
-		} else { // opposing team scored
+			playerFeedback.innerText = "SCORE!!!";
+		// opposing team scored
+		} else {
 			pubnub.publish({
 				channel: gameChannel,
 				message: {
@@ -149,7 +140,7 @@ $(document).ready(function() {
 					team: (team === blueTeam ? redTeam : blueTeam)
 				}
 			})
-			$('#player-feedback').html("LOST!!!");
+			playerFeedback.innerText = "OPPOSING TEAM SCORED!";
 		}
 
 		// reset game		
@@ -159,27 +150,20 @@ $(document).ready(function() {
 	function calculateMoveDistance() {
 		if (gameOptions.randomMoveDistance) {
 			var multiplier = _.random(1, 4) 
-			return baseMoveDistance * multiplier;
+			return defaultMoveDistance * multiplier;
 		} else {
-			return baseMoveDistance;	
+			return defaultMoveDistance;	
 		}		
 	}
 
-	function showMoveFeedback(success) {				
-		if (success) {
-			$('#player-feedback').html("SUCCESS! ...for now")
-		} else {
-			$('#player-feedback').html("you have FAILED... :)")			
-		}
-	}
-
-	function placeBall() {		
-		$ball.css("left", (board.width / 2) - ($ball.width()/2))		
+	
+	function placeBall() {
+		ball.style.left = (board.width / 2) - (ball.offsetWidth / 2)		
 	}
 
 	function displayCommand() {
 		var commandIndex = _.random(0, commandsPool.length - 1);
-		$('.command').html(commandsPool[commandIndex]);
+		PUBNUB.$('command').textContent = (commandsPool[commandIndex]);
 		setTimeout(function() {
 			displayCommand();
 		}, intervalBetweenCommands())
@@ -194,31 +178,54 @@ $(document).ready(function() {
 		}		
 	}
 
-	function initGame() {
-		$(".command-input").focus();
-		$(".command-input").on('keyup', function(e) {
-		if ((e.keyCode || e.charCode) === 13) {
-			//console.log($(".command-input").val() === $(".command").html())
-			if ($(".command-input").val() === $(".command").html()) {
-				showMoveFeedback(true);
-				sendMoveBallMessage(true);
-			} else {
-				showMoveFeedback(false);
-				sendMoveBallMessage(false);
-			}
-			$(".command-input").val("");
+	function getMoveDistance(status) {		
+		var moveDistance;
+		if (team === blueTeam) {			
+			moveDistance = status.success ? 0-calculateMoveDistance() : 0+calculateMoveDistance();
+		} else {
+			moveDistance = status.success ? 0+calculateMoveDistance() : 0-calculateMoveDistance();		
 		}
-	})
+		return moveDistance;
 	}
 
+	function successMove() {		
+		playerFeedback.innerText = "Success!"
+		broadcastMove(getMoveDistance({ success: true }))
+	}
+
+	function unsuccessfulMove() {		
+		playerFeedback.innerText = "Try again :)"
+		broadcastMove(getMoveDistance({ success: false }))
+	}
+
+	function broadcastMove(moveDistance) {		
+		pubnub.publish({
+			channel: gameChannel,
+			message: {
+				type: "ballmove",
+				value: moveDistance
+			}
+		})
+	}
+
+	function initGameAndEvents() {			
+		PUBNUB.bind('keyup', commandInput, function(e) {
+			if((e.keyCode || e.charCode) === 13) {
+				(commandInput.value === command.innerHTML) ? successMove() : unsuccessfulMove()
+				commandInput.value = ""
+			}
+		});
+
+		PUBNUB.bind('resize', window, function() { calculateBoardAttributes() })
+		commandInput.focus();
+	}	
 
 	assignTeam(); 
-	initGame();
+	initGameAndEvents();	
+	calculateBoardAttributes();
 	placeBall();
 	
 	setTimeout(function() {
 		displayCommand();
-	}, 4000)
-
-// })($)
-});
+	}, timeToGameStart)
+}, 50)
